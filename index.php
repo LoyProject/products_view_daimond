386ddecd9562d5cac27abb0f70c859ef8e482bfe
beounvichet <43592647+beounvichet@users.noreply.github.com>
2025-03-18 15:18:08 +0700

    update

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Diamond</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@latest/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -234,7 +235,7 @@
 
 <!-- ✅ MODAL HTML -->
 <div id="productModal" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-    <div class="bg-white rounded-lg p-6 max-w-3xl w-full shadow-lg relative"> <!-- Increased max-width to 3xl -->
+    <div class="bg-white rounded-lg p-6 max-w-2xl w-full shadow-lg relative"> <!-- Increased max-width to 3xl -->
     <div id="productGallery" class="relative w-full h-96 overflow-hidden rounded"> <!-- Increased height to 96 -->
             <img id="modalImage" src="" class="w-full h-full object-cover">
             <button id="prevImage" class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-2 py-1 rounded">‹</button>
@@ -361,7 +362,7 @@
 </script>
 
     <footer class="p-4 bg-gray-200 mt-6">
-        <p class="text-center text-gray-600">© 2025 Loy Team. All rights reserved.</p>
+        <p class="text-center text-gray-600">© 2025 <a href="https://fb.com/khmerpos2" class="text-blue-500 hover:underline">Simple POS</a> All rights reserved.</p>
     </footer>
 
 </body>
